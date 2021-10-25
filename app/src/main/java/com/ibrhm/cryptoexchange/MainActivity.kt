2732a57eaf96e9de0067504bdbaa8104f7d803ea
İbrahim <iuyar99@gmail.com>
2021-10-25 04:01:15 -0700

    style: layout design for home fragment

--- app/src/main/java/com/ibrhm/cryptoexchange/MainActivity.kt
+++ app/src/main/java/com/ibrhm/cryptoexchange/MainActivity.kt
@@ -2,12 +2,24 @@ package com.ibrhm.cryptoexchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavBar)
+        val navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 }
