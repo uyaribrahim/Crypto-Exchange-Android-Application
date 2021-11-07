1bd38025746c21118a1c06d7e95b93519b6f9167
İbrahim <iuyar99@gmail.com>
2021-11-07 03:31:50 -0800

    refactoring

--- app/src/main/java/com/ibrhm/cryptoexchange/view/HomeFragment.kt
+++ app/src/main/java/com/ibrhm/cryptoexchange/view/HomeFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.ibrhm.cryptoexchange.R
+import com.ibrhm.cryptoexchange.viewmodel.MarketsViewModel
+import kotlinx.android.synthetic.main.fragment_home.*
+import java.text.NumberFormat
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +24,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
+
+    private lateinit var marketViewModel: MarketsViewModel
+    private var numberFormat = NumberFormat.getCurrencyInstance(Locale.US)
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -28,6 +39,28 @@ class HomeFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        numberFormat.maximumFractionDigits = 0
+
+        marketViewModel = ViewModelProvider(this).get(MarketsViewModel::class.java)
+
+        marketViewModel.getCurrencyData()
+
+        marketViewModel.coinList.observe(viewLifecycleOwner, Observer {
+
+            btc_price.text = numberFormat.format(it[0].price)
+            eth_price.text = numberFormat.format(it[1].price)
+            sol_price.text = numberFormat.format(it[3].price)
+
+            btc_volume.text = numberFormat.format(it[0].volume_24h)
+            eth_volume.text = numberFormat.format(it[1].volume_24h)
+            sol_volume.text = numberFormat.format(it[3].volume_24h)
+
+        })
     }
 
     override fun onCreateView(
